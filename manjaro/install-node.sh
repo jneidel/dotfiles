b0d5b234d738068f5bb1a74c9fdc295635e95a6b
Jonathan Neidel <jneidel@outlook.com>
2018-06-22 09:46:55 +0200

    Add node install script

---
+++ manjaro/install-node.sh
@@ -0,0 +1,15 @@
+NODEV=10.5.0
+echo "installing node v"$NODEV;
+wget https://nodejs.org/dist/v$NODEV/node-v$NODEV-linux-x64.tar.xz -O ~/.bin/node-latest.tar.xz; # node current linux binaries
+cd ~/.bin;
+tar xf node-latest.tar.xz;
+echo "tar xf";
+rm node-latest.tar.xz;
+mv node-v$NODEV-linux-x64 node-$NODEV;
+ln -sf node-$NODEV/bin/node node;
+echo "created node symlink";
+ln -sf node-$NODEV/lib/node_modules/npm/bin/npm-cli.js npm;
+echo "created npm symlink";
+ln -sf node-$NODEV/lib/node_modules/npm/bin/npx-cli.js npx;
+echo "created npx symlink";
+
