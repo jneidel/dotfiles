407d9104fcac3bdc71fe6be4ea2c09a715a72c71
Jonathan Neidel <github@jneidel.com>
2019-03-15 11:57:26 +0100

    Add old tmux config

--- server/install.sh
+++ server/install.sh
@@ -1,8 +1,11 @@
 D=https://raw.githubusercontent.com/jneidel/dotfiles/master
 S=$D/server
 
-echo "## Installing tmux config ##"
+echo "### Installing tmux configs ###"
 wget -q $S/.tmux.conf -O ~/.tmux.conf
+# wget -q $S/.tmux.conf-1.8 -O ~/.tmux.conf # Use if your tmux version is 1.x
+echo "#### Wrote ~/.tmux.conf"
 mkdir -p ~/.tmux
 wget -q $S/.tmux/maximize-pane -O ~/.tmux/maximize-pane
+echo "#### Wrote ~/.tmux/maximize-pane"
 
