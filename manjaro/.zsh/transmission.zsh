586b0e41948d1a09a8c274cc9d04ff79f0563b08
Jonathan Neidel <git@jneidel.com>
2019-06-20 14:53:51 +0200

    Add light themes, update mpd scripts

--- manjaro/.zsh/transmission.zsh
+++ manjaro/.zsh/transmission.zsh
@@ -2,27 +2,28 @@
 ## based on gotbletus setup: https://github.com/gotbletu/shownotes/blob/master/transmission-cli.txt
 ## blocklist: https://www.iblocklist.com/list?list=ydxerpxkpcfqjaybcssw
 
-alias tsm="transmission-remote -l" # list torrents
+# tsr is a tsr wrapper, which starts the daemon if it's not already running
+alias tsm="tsr -l" # list torrents
 
-alias tsmadd="transmission-remote -a" # add torrent
+alias tsmadd="tsr -a" # add torrent
 alias tsma="tsmadd"
 
-tsmpause() { transmission-remote -t"$1" --stop; }  # stop id or all
+tsmpause() { tsr -t"$1" --stop; }  # stop id or all
 alias tsmp="tsmpause"
 
-tsmstart() { transmission-remote -t"$1" --start; } # start id or all
+tsmstart() { tsr -t"$1" --start; } # start id or all
 alias tsms="tsmstart"
 
 tsminfo() { # display torrent info
   tsm -t $1 -i G -P "Name:|Total size:|Downloaded:|Peers:"
 }
 alias tsmi="tsminfo"
-# tsminfo() { transmission-remote -t"$1" --info; } # has a lot more information
+# tsminfo() { tsr -t"$1" --info; } # has a lot more information
 
-tsmrm() { transmission-remote -t"$1" --remove; } # delete torrent
-tsmpurge() { transmission-remote -t"$1" --remove-and-delete; } # delete torrent + data
+tsmrm() { tsr -t"$1" --remove; } # delete torrent
+tsmpurge() { tsr -t"$1" --remove-and-delete; } # delete torrent + data
 
-alias tsmwatch="watch -t -n .7 transmission-remote -l" # watch mode, updating 'tsm'
+alias tsmwatch="watch -t -n .7 tsr -l" # watch mode, updating 'tsm'
 alias tsmw="tsmwatch"
 
 alias tsmdone='tsm G "Done" G -v "None" G -v "ETA" HL "done"' # list finished torrents
@@ -35,7 +36,7 @@ alias tsmsite="$BROWSER http://localhost:9091" # open transmission site
 alias tsmdaemon="transmission-daemon -c ~/Downloads" # start daemon
 
 tsmget() {
-  transmission-remote -t $1 # add -G, -g depending on whenever or not to get
+  tsr -t $1 # add -G, -g depending on whenever or not to get
 }
 alias tsmg="tsmget"
 
