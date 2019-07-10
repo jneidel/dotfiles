304f8a2495da3e96cb1d0f27e79b3a3e8f78ee8f
Jonathan Neidel <jneidel@jneidel.com>
2019-07-10 13:49:09 +0200

    Update status network script

--- manjaro/scripts/status/i3-lemonbar/network.sh
+++ manjaro/scripts/status/i3-lemonbar/network.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+# This script gets fed a stream of patterns that matched an entry in the
+# journalctl log of NetworkManager and mullvad-daemon.
+# The patterns to match are found in 'network-patterns'.
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -13,9 +16,9 @@ while read -r line; do
       VPN=1 ;;
     "New tunnel state: Disconnected")
       VPN=0 ;;
-    "wlp3s0): Activation: successful")
+    "wlp3s0): Activation: successful"|"NetworkManager state is now CONNECTED_GLOBAL")
       WIFI=1 ;;
-    "wlp3s0): state change: deactivating -> disconnected")
+    "wlp3s0): state change: deactivating -> disconnected"|"NetworkManager state is now DISCONNECTED"|"NetworkManager state is now CONNECTED_LOCAL"|"NetworkManager state is now CONNECTED_SITE"|"NetworkManager state is now CONNECTING")
       WIFI=0 ;;
   esac
   echo "NET$WIFI $VPN"
