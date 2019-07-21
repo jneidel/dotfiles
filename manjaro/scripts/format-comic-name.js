fad98137cb6d973a545187f01cb87922c60fb90f
Jonathan Neidel <jneidel@jneidel.com>
2019-07-21 16:35:56 +0200

    Cleanup scripts, update/add scripts

--- manjaro/scripts/format-comic-name.js
+++ manjaro/scripts/format-comic-name.js
@@ -6,10 +6,11 @@
  * Input format:  Oblivion Song 011 (2019) (Digital) (Zone-Empire).cbr
  * Output format: oblivion-song-011.cbr
  *
- * See rncomic for usage information
+ * See rename-comic for usage information
  */
 
 const nameRaw = process.argv[2];
+const onlySeries = process.argv[3] == "nameonly"
 
 function createName( name, issue ) {
   name = name.toLowerCase();
@@ -27,9 +28,14 @@ name = completePath.pop();
 const [ , seriesName, issue ] = name.match( /^(.*?)\s(\d\d\d)/ );
 
 const formattedName = `${createName( seriesName, issue )}.${nameExt}`;
-completePath.push( formattedName );
 
-const result = completePath.join( "/" );
+// If you want the complete path converted use result instead of formattedName:
+// completePath.push( formattedName );
+// const result = completePath.join( "/" );
 
-console.log( result );
+if ( !onlySeries ) {
+  console.log( formattedName );
+} else {
+  console.log( seriesName.toLowerCase().split( " " ).join( "-" ) )
+}
 
