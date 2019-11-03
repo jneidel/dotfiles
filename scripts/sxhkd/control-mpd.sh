6f5fbd61a2532a143c0336691588ec5358554ea9
Jonathan Neidel <jneidel@jneidel.com>
2019-11-03 15:40:51 +0100

    Update sxhkd scripts

--- scripts/sxhkd/control-mpd.sh
+++ scripts/sxhkd/control-mpd.sh
@@ -1,21 +1,64 @@
 #! /bin/sh
 
 if [ "$1" = "--help" ] || [ "$1" = "-h" ] || [ "$1" = "help" ]; then
-  printf "$ control-mpd.sh <++>
-<++>
+  printf "$ control-mpd.sh <command>
+Send commands to mpd and send a notification on it.
+For usage via keyboard shortcuts (sxhkd).
 
-Parameters:
-  \$1: <++>
-  [\$1]: <++>
+Commands:
+  toggle: pause/play music
+  next: next track
+  prev: previous track
 
 Example:
-  $ control-mpd.sh <++>
+  $ control-mpd.sh toggle
 "
   exit
 fi
 
 command -v mpcc >/dev/null || { echo "mpcc script is not in PATH"; exit 1; }
+command -v mpc-get >/dev/null || { echo "mpc-get script is not in PATH"; exit 1; }
+command -v is-mpd-playing >/dev/null || { echo "is-mpd-playing script is not in PATH"; exit 1; }
 
-# notify-send ok
-mpcc toggle
+mpcc_exec() {
+  mpcc $1 > /dev/null
+}
+
+notification() {
+  # $1 icon
+  # $2 head
+
+  TRACK="$(mpc-get '%title% - %artist%')"
+  [ "$TRACK" = " - " ] && {
+    TRACK="basename '$(mpc-get file)'"
+  }
+
+  notify-send -i $1 -h string:x-canonical-private-synchronous:controlMpd -u normal -t 7000 "$2" "$TRACK"
+}
+
+toggle() {
+  if is-mpd-playing; then
+    notification player_pause "Pause" &
+  else
+    notification player_play "Play" &
+  fi
+
+  mpcc_exec toggle
+}
+
+next() {
+  notification player_end "Next" &
+  mpcc_exec next
+}
+
+prev() {
+  notification player_start "Previous" &
+  mpcc_exec prev
+}
+case $1 in
+  toggle) toggle;;
+  next) next;;
+  prev) prev;;
+  *) echo "Please pass either 'toggle', 'next' or 'prev'";;
+esac
 
