c41adb355c153e5a0f67f37a2772876589ec6b58
Jonathan Neidel <jneidel@jneidel.com>
2020-03-15 15:26:10 +0100

    Add pi setup script

---
+++ install-scripts/pi.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ "$1" = "--help" ] || [ "$1" = "-h" ] || [ "$1" = "help" ]; then
+  printf "$ pi.sh
+Install dotfiles for a raspberry pi
+"; exit
+fi
+
+echo "Copy scripts"
+mkdir -p ~/scripts
+cd $DIR/../scripts
+cp mount/mo mpd/ncmpcpps mpd/send-cover ~/scripts
+
+echo "Copy zshrc"
+cd $DIR/..
+mkdir -p ~/.zsh ~/.z
+cp -r .zsh/oh-my-zsh.sh .zsh/lib .zsh/cobalt2.zsh-theme ~/.zsh/
+cp other/pi-zshrc ~/.zshrc
+
+echo "Copy mpd configs"
+cd $DIR/..
+mkdir -p ~/.config/mpd ~/.config/ncmpcpp
+cp other/pi-mpd.conf ~/.config/mpd/mpd.conf
+cp .config/ncmpcpp/config .config/ncmpcpp/bindings ~/.config/ncmpcpp/
+
