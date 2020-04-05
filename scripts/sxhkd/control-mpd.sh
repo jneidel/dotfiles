e8377e2d786be3f2af05ed15edadd81abe82f582
Jonathan Neidel <jneidel@jneidel.com>
2020-04-05 21:29:20 +0200

    Optimize sxhkd mpd script

--- scripts/sxhkd/control-mpd.sh
+++ scripts/sxhkd/control-mpd.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+TMP=/tmp/control-mpd-tmp
+
 if [ "$1" = "--help" ] || [ "$1" = "-h" ] || [ "$1" = "help" ]; then
   printf "$ control-mpd.sh <command>
 Send commands to mpd and send a notification on it.
@@ -17,48 +19,40 @@ Example:
 fi
 
 command -v mpcc >/dev/null || { echo "mpcc script is not in PATH"; exit 1; }
-command -v mpc-get >/dev/null || { echo "mpc-get script is not in PATH"; exit 1; }
-command -v is-mpd-playing >/dev/null || { echo "is-mpd-playing script is not in PATH"; exit 1; }
 
-mpcc_exec() {
-  mpcc $1 > /dev/null
-}
+run_cmd() {
+  CMD="$1"
 
-notification() {
-  # $1 icon
-  # $2 head
+  mpcc $CMD -f 'TRACK:%title% - %artist%\nFILE:%file%' > $TMP
 
-  TRACK="$(mpc-get '%title% - %artist%')"
+  TRACK="$(grep -Po '^TRACK:\K.*' $TMP)"
   [ "$TRACK" = " - " ] && {
-    TRACK="$(basename "$(mpc-get file)" | rev | cut -d. -f2 | rev)"
+    TRACK="$(basename "$(grep -Po '^FILE:\K.*' $TMP)" | rev | cut -d. -f2 | rev)"
   }
 
-  notify-send -i $1 -h string:x-canonical-private-synchronous:controlMpd -u normal -t 7000 "$2" "$TRACK"
-}
-
-toggle() {
-  if is-mpd-playing; then
-    notification player_pause "Pause" &
-  else
-    notification player_play "Play" &
+  if [ "$CMD" = "toggle" ]; then
+    if grep "^\[playing" $TMP; then
+      ICON=player_play
+      HEAD=Play
+    else
+      ICON=player_pause
+      HEAD=Pause
+    fi
   fi
 
-  mpcc_exec toggle
+  notify-send -i $ICON -h string:x-canonical-private-synchronous:controlMpd -u normal -t 7000 "$HEAD" "$TRACK"
 }
 
-next() {
-  notification player_end "Next" &
-  mpcc_exec next
-}
-
-prev() {
-  notification player_start "Previous" &
-  mpcc_exec prev
-}
 case $1 in
-  toggle) toggle;;
-  next) next;;
-  prev) prev;;
+  toggle) run_cmd toggle;;
+  next)
+    ICON=player_end
+    HEAD=Next
+    run_cmd next;;
+  prev)
+    ICON=player_start
+    HEAD=Previous
+    run_cmd prev;;
   *) echo "Please pass either 'toggle', 'next' or 'prev'";;
 esac
 
