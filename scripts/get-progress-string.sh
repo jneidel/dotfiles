0fdd41aa25a96af97d6cd4463f7087b419810fe5
Jonathan Neidel <jneidel@jneidel.com>
2020-05-10 17:39:13 +0200

    Optimize volume control script

--- scripts/get-progress-string.sh
+++ scripts/get-progress-string.sh
@@ -1,21 +1,31 @@
-#!/bin/bash
-# getProgressString <TOTAL ITEMS> <FILLED LOOK> <NOT FILLED LOOK> <STATUS>
-# For instance:
-# $ getProgressString 10 "#" "-" 50
-# #####-----
-# Note: if you want to use | in your progress bar string you need to change the delimiter in the sed commands
+#!/bin/sh
 
-ITEMS="$1" # The total number of items(the width of the bar)
-FILLED_ITEM="$2" # The look of a filled item
-NOT_FILLED_ITEM="$3" # The look of a not filled item
-STATUS="$4" # The current progress status in percent
-# \\\033[1;34m
+if [ "$1" = "--help" ] || [ "$1" = "-h" ] || [ "$1" = "help" ] || [ -z "$4" ]; then
+  cat << EOF
+$ get-progress-string.sh ITEMS FILLED UNFILLED PERCENTAGE
+Generate a progress bar
 
-# calculate how many items need to be filled and not filled
-FILLED_ITEMS=$((${ITEMS} * ${STATUS}/100))
+Parameters:
+  \$1: number of total items in the bar
+  \$2: look of filled items
+  \$3: look of unfilled items
+  \$4: percentage of items filled
+
+Example:
+  $ get-progress-string.sh 10 '▪' '-' 50
+  #-> ▪▪▪▪▪-----
+EOF
+  exit
+fi
+
+ITEMS="$1"
+FILLED_ITEM="$2"
+NOT_FILLED_ITEM="$3"
+PERCENTAGE="$4"
+
+FILLED_ITEMS=$((${ITEMS} * ${PERCENTAGE}/100))
 NOT_FILLED_ITEMS=$(($ITEMS - $FILLED_ITEMS))
 
-# Assemble the bar string
-msg=$(printf "%${FILLED_ITEMS}s" | sed "s| |${FILLED_ITEM}|g")
-msg=${msg}$(printf "%${NOT_FILLED_ITEMS}s" | sed "s| |${NOT_FILLED_ITEM}|g")
-printf "$msg\n"
+filled=$(printf "%${FILLED_ITEMS}s" | sed "s: :${FILLED_ITEM}:g")
+unfilled=${bar}$(printf "%${NOT_FILLED_ITEMS}s" | sed "s: :${NOT_FILLED_ITEM}:g")
+printf "${filled}${unfilled}\n"
