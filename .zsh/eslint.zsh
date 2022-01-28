d8667e189d4969ed7ecf44558597dd87d727e15c
Jonathan Neidel <jneidel@jneidel.com>
2022-01-28 15:44:02 +0100

    Update eslint to latest

--- .zsh/eslint.zsh
+++ .zsh/eslint.zsh
@@ -4,8 +4,13 @@
 alias fix="eslint --fix"
 alias fixa="eslint . --fix"
 
-alias fixt="eslint --ext '.ts' --config ~/.eslintrc-ts --fix"
-alias fixta="eslint --ext '.ts' --config ~/.eslintrc-ts . --fix"
+# alias fixt="eslint --ext '.ts' --config ~/.config/eslint/eslintrc-ts --parser-options \"{'tsconfigRootDir':\"$(git rev-parse --show-toplevel)\"}\" --fix"
+gitRoot() {
+}
+tfix() {
+  local gitRoot="$(git rev-parse --show-toplevel)"
+  eslint --ext '.ts' --config ~/.config/eslint/eslintrc-ts --parser-options "{'tsconfigRootDir':\"$gitRoot\"}" "$gitRoot" --resolve-plugins-relative-to=/usr/lib/node_modules --fix
+}
 
 ## create markdown toc
 alias toc="markdown-toc -i readme.md"
