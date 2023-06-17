b7fe947cbe2460e8c2488a9d3bcf92753449ffef
Jonathan Neidel <jneidel@jneidel.com>
2023-06-17 12:47:27 +0200

    Configure neorg metagen/summary modules

--- .config/nvim/lua/plugin/neorg.lua
+++ .config/nvim/lua/plugin/neorg.lua
@@ -63,8 +63,7 @@ if hasmodule then
             local leader = keybinds.leader
 
             -- https://github.com/nvim-neorg/neorg/wiki/Todo-Items
-            keybinds.remap_event("norg", "n", "gtd", "core.qol.todo_items.todo.task_done")
-            keybinds.remap_event("norg", "n", "gtu", "core.qol.todo_items.todo.task_undone")
+            keybinds.remap_event("norg", "n", "gtd", "core.qol.todo_items.todo.task_done") keybinds.remap_event("norg", "n", "gtu", "core.qol.todo_items.todo.task_undone")
             keybinds.remap_event("norg", "n", "gtp", "core.qol.todo_items.todo.task_pending")
             keybinds.remap_event("norg", "n", "gth", "core.qol.todo_items.todo.task_on_hold")
             keybinds.remap_event("norg", "n", "gtc", "core.qol.todo_items.todo.task_cancelled")
@@ -103,6 +102,22 @@ if hasmodule then
           close_after_use = true
         }
       },
+      ["core.esupports.metagen"] = {
+        -- https://github.com/nvim-neorg/neorg/wiki/Metagen
+        config = {
+          type = "auto",
+          template = {
+            -- src: https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/esupports/metagen/module.lua
+            { "title", function() return vim.fn.expand("%:p:t:r") end },
+            { "description", "<++>" },
+            { "categories", "<++>"},
+            { "authors", function() return require("neorg.external.helpers").get_username() end },
+            { "created", function() return os.date("%Y-%m-%d") end },
+            { "updated", function() return os.date("%Y-%m-%d") end },
+          }
+        }
+      },
+      ["core.summary"] = {}, -- https://github.com/nvim-neorg/neorg/wiki/Summary
       ["core.ui.calendar"] = {},
       -- ["core.export"] = {},
       --       ["core.export.markdown"] = {
