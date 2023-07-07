b7087975d5914a7c8325e2b24d8e1e3e3c9a75ef
Jonathan Neidel <jneidel@jneidel.com>
2023-07-07 14:52:15 +0200

    Update telescope file finder

--- .config/nvim/lua/plugin/telescope.lua
+++ .config/nvim/lua/plugin/telescope.lua
@@ -21,6 +21,7 @@ return {
     local telescope = require("telescope")
     local actions = require("telescope.actions")
     local builtin = require('telescope.builtin')
+    local HOME = os.getenv("HOME")
 
     if vim.fn.has( "macunix" ) then
       vim.cmd( "set rtp+=/usr/local/opt/fzf" )
@@ -51,9 +52,10 @@ return {
         },
         file_ignore_patterns = {
           "node_modules/", "vendor/", ".git/", ".mp3", ".pdf", ".png", ".jpg", ".jpeg",
-          ".mkv", ".mp4", ".zip", ".ods", ".m3u", "git/", ".cache", 'teams-for-linux/',
+          ".mkv", ".avi", ".flac", ".mp4", ".zip", ".ods", ".m3u", ".url", "git/", ".cache",
           '.local/lib', '.local/share/*/', 'BraveSoftware/', 'chromium', 'chrome/', 'firefox/',
           '.vim/sessions', '.vim/tmp', '.vim/backup', 'Signal', 'tmp/', 'ct/manga', 'ct/comics',
+          ".cbr", "cbz", 'teams-for-linux/', ".aac", ".mpc", ".epub", ".sql", ".ydb", ".JPG", ".xlsx",
         },
       },
       extensions = {
@@ -65,30 +67,62 @@ return {
         }
       },
     }
-    telescope.load_extension('fzf')
 
-    -- general file finder
-    if vim.fn.getcwd() == os.getenv("HOME") then
+    local function register_fzf_find_files()
+      telescope.load_extension('fzf')
+
       -- fzf as alternative for the home directoy, as telescope is very slow there
-      vim.cmd( "let $FZF_DEFAULT_COMMAND = \"rg --ignore --hidden --files --type-add='exclude:*.{mp3,png,jpg,jpeg,pdf,mkv,mp4,avi,zip,ods,m3u}' --type-not=exclude --glob='!.git/' --glob '!git/' --glob '!node_modules/' --glob '!.cache' --glob '!vendor/' --glob '!teams-for-linux/' --glob '!.local/lib' --glob '!.local/share/*/' --glob '!BraveSoftware/' --glob '!chromium' --glob '!chrome/' --glob '!firefox/' --glob '!Signal' --glob '!.ib-tws/' --glob '!.java/' --glob '!.zcompcache' --glob '!tmp/' --glob='!ct/manga' --glob='!ct/comics' --glob='!dotfiles/' -g='!coverage' -g='!dist/' -g='!build/' -g='!.gradle/'\"" )
+      vim.cmd( "let $FZF_DEFAULT_COMMAND = \"rg --ignore --hidden --files --type-add='exclude:*.{mp3,png,jpg,JPG,jpeg,pdf,mkv,mp4,avi,zip,ods,xlsx,m3u,url,aac,mpc,epub,sql,ydb}' --type-not=exclude --glob='!.git/' --glob '!git/' --glob '!node_modules/' --glob '!.cache' --glob '!vendor/' --glob '!teams-for-linux/' --glob '!.local/lib' --glob '!.local/share/*/' --glob '!BraveSoftware/' --glob '!chromium' --glob '!chrome/' --glob '!firefox/' --glob '!Signal' --glob '!.ib-tws/' --glob '!.java/' --glob '!.zcompcache' --glob '!tmp/' --glob='!ct/manga' --glob='!ct/comics' --glob='!dotfiles/' -g='!coverage' -g='!dist/' -g='!build/' -g='!.gradle/'\"" )
+      vim.cmd( "let $FZF_DEFAULT_OPTS = \"--bind TAB:up,Shift-TAB:down\"" )
 
       vim.keymap.set("n", "<A-f>", ":Files<CR>", {});
       vim.keymap.set("n", "<A-s>", ":sp<CR> :Files<CR>", {});
       vim.keymap.set("n", "<A-v>", ":vsp<CR> :Files<CR>", {});
       vim.keymap.set("n", "<A-t>", ":tabe<CR> :Files<CR>", {});
-    else
-      local function custom_find_files()
-        vim.fn.system('git rev-parse --is-inside-work-tree')
-        if vim.v.shell_error == 0 then
-          builtin.git_files()
-        else
-          builtin.find_files({
-            find_command = { "rg", "--files", "--hidden", "--ignore" },
-          })
-        end
+    end
+
+    local function use_git_find_files_if_git_repo()
+      local function is_git_repo()
+        vim.fn.system("git rev-parse --is-inside-work-tree")
+        return vim.v.shell_error == 0
+      end
+      local function get_git_root()
+        local dot_git_path = vim.fn.finddir(".git", ".;")
+        return vim.fn.fnamemodify(dot_git_path, ":h")
       end
 
-      vim.keymap.set("n", "<A-f>", custom_find_files, {});
+      local opts = {
+        find_command = { "rg", "--files", "--hidden", "--ignore" },
+        prompt_title = "Find in git",
+      }
+      -- if is_git_repo() then
+      --   opts.cwd = get_git_root()
+      -- end
+      builtin.find_files( opts )
+
+      -- vim.fn.system('git rev-parse --is-inside-work-tree')
+      -- if vim.v.shell_error == 0 then
+      --   builtin.git_files()
+    end
+
+    local function find_files_in_dir( dir, title )
+      builtin.find_files({
+        find_command = { "rg", "--files", "--hidden", "--ignore" },
+        prompt_title = "Find in "..title,
+        cwd = dir,
+      })
+    end
+    local function find_in_org()
+      find_files_in_dir( HOME.."/org", "org" )
+    end
+
+    -- general file finder
+    if vim.fn.getcwd() == HOME then
+      register_fzf_find_files()
+    elseif string.find( vim.fn.getcwd(), HOME.."/org" ) then
+      vim.keymap.set("n", "<A-f>", find_in_org, {});
+    else
+      vim.keymap.set("n", "<A-f>", use_git_find_files_if_git_repo, {});
     end
     vim.keymap.set("n", "<A-F>", builtin.buffers, {});
 
