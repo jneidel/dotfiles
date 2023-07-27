fc960d2645dee0a2a5503d28da08485f43d51743
Jonathan Neidel <jneidel@jneidel.com>
2023-07-27 18:41:37 +0200

    Refactor zsh init file for more dynamic importing

--- .zsh/init.zsh
+++ .zsh/init.zsh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
-# env vars first
-for env_file in $(ls $ZSH_DIR/*.env); do
+## env variables
+test -e "$ZSH_DIR/org.env" && source $ZSH_DIR/org.env
+for env_file in $(find $ZSH_DIR -type f -name '*.env' -not -path "$ZSH_DIR/org.env"); do
   source $env_file
 done
-source ~/scripts/colored-man.env
+test -e "~/scripts/colored-man.env" && source ~/scripts/colored-man.env
 
-# aliases second
-for alias_file in $(ls $ZSH_DIR/*.zsh | grep -v "init.zsh"); do
+## aliases
+for alias_file in $(find $ZSH_DIR -type f -name '*.zsh' -not -path "$ZSH_DIR/lib/*" -not -path "$ZSH_DIR/init.zsh"); do
   source $alias_file
 done
