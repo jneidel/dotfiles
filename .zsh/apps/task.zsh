0c5a6bc7cecfe0f90e1e1c261ecd7191d0be1c5c
Jonathan Neidel <jneidel@jneidel.com>
2023-08-14 13:36:10 +0200

    feat(task): add taskwarrior

---
+++ .zsh/apps/task.zsh
@@ -0,0 +1,12 @@
+alias t="task"
+
+# reports
+alias tn="task next"
+alias tl="task list"
+alias ta="task all '(status:waiting or status:pending)'"
+
+# crud
+alias td="task done"
+
+# org
+alias ti="task add tag:inbox"
