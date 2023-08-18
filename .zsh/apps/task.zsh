895b1759f2f5a03b0709246f0d3fb696a2bb1e41
Jonathan Neidel <jneidel@jneidel.com>
2023-08-18 20:43:20 +0200

    feat(task): adjust rc and aliases

--- .zsh/apps/task.zsh
+++ .zsh/apps/task.zsh
@@ -1,12 +1,20 @@
-alias t="task"
+t() {
+  if [ -n "$1" ]; then
+    task "$@"
+  else
+    task ready
+  fi
+}
 
 # reports
 alias tn="task next"
 alias tl="task list"
 alias ta="task all '(status:waiting or status:pending)'"
+alias tre="task ready"
 
 # crud
 alias td="task done"
+alias ts="task start"
 
 # org
 alias ti="task add tag:inbox"
