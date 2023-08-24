7bbfc70d09cfd1f0dcbfdb94757f56bc5c87c000
Jonathan Neidel <jneidel@jneidel.com>
2023-08-24 21:31:05 +0200

    feat(ledger): add aliases for pots & asset reports

--- .zsh/apps/ledger.zsh
+++ .zsh/apps/ledger.zsh
@@ -1,1 +1,6 @@
 alias l="hledger -s"
+
+# reports
+alias -g NOFLOW="--color=always | grep -ve income -e opening -e expenses -e XMR | head -n-2" # no money flow
+alias pots="l bal NOFLOW"
+alias ass="l bal -R NOFLOW"
