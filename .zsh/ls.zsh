ea491a6355fa3b80d6f01304b7820f88f69a951b
Jonathan Neidel <jneidel@jneidel.com>
2023-09-08 10:36:49 +0200

    feat(util): replace deprecated exa with eza

--- .zsh/ls.zsh
+++ .zsh/ls.zsh
@@ -1,8 +1,8 @@
 export EXA_COLORS="di=1;34" # make dirs bold again
 
-alias ls="exa --git-ignore"
-alias la="exa -1a --git-ignore" # all
-alias ld="exa -1D" # only dirs
-alias li="exa -lah --git" # show permissions
+alias ls="eza --time-style=relative --git-ignore"
+alias la="eza --time-style=relative -1a --git-ignore" # all
+alias ld="eza --time-style=relative -1D" # only dirs
+alias li="eza --time-style=relative -lah --git" # show permissions
 alias lid="echo 'Sorted by modified date. Oldest to newest.'; li -s modified"
 alias sl="ls" # typo correction
