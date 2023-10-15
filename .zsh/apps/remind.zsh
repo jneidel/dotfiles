f2ccb16ebb41382cc905fde952343c7b1443c617
Jonathan Neidel <jneidel@jneidel.com>
2023-10-15 10:34:19 +0200

    feat(remind): upgrade aliases for easier calendar access

--- .zsh/apps/remind.zsh
+++ .zsh/apps/remind.zsh
@@ -1,14 +1,18 @@
 export DOTREMINDERS=$ORG_CALENDAR/main
 
-alias mc="clear && rem -b1 -cu+2 -m -@2,0 2>/dev/null"
+mc() {
+  n=2
+  if [ -n "$1" ]; then
+    n=$1
+  fi
+  clear && rem -b1 -cu+$n -m -@2,0 2>/dev/null
+}
 alias mcm="clear && rem -b1 -cu+4 -m -@2,0 2>/dev/null"
 alias mcmm="clear && rem -b1 -cu+6 -m -@2,0 2>/dev/null"
-alias mc4=mcm
-alias mc6=mcmm
-alias mc8="clear && rem -b1 -cu+8 -m -@2,0 2>/dev/null"
-alias mc12="clear && rem -b1 -cu+12 -m -@2,0 2>/dev/null"
-alias mc14="clear && rem -b1 -cu+14 -m -@2,0 2>/dev/null"
-alias mc16="clear && rem -b1 -cu+16 -m -@2,0 2>/dev/null"
+
+for i in {1..16}; do
+  alias mc$i="clear && rem -b1 -cu+$i -m -@2,0 2>/dev/null"
+done
 alias mce='rem -b1 -cu+2 -m -@2,0 2>&1 | grep -ve "│" -e "trigger" -e "┬" -e "┼" -e "┴"'
 
 events() {
