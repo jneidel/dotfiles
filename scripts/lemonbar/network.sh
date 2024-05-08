120d9814dfe9f73b0c5620adf4eb3315d2019169
Jonathan Neidel <jneidel@jneidel.com>
2024-05-08 02:31:54 +0200

    chore(misc): add network patterns for a new vpn

--- scripts/lemonbar/network.sh
+++ scripts/lemonbar/network.sh
@@ -13,9 +13,9 @@ echo "NET$WIFI $VPN $HOMENET"
 
 while read -r line; do
   case "$line" in
-    "New tunnel state: Connected")
+    "New tunnel state: Connected"|"device (AirVPN-eu): Activation: successful, device activated.")
       VPN=1 ;;
-    "New tunnel state: Disconnected"|"Error: Failed to set DNS")
+    "New tunnel state: Disconnected"|"Error: Failed to set DNS"|"device (AirVPN-eu): state change: "*" -> disconnected")
       VPN=0 ;;
     "(w*): Activation: successful"|"NetworkManager state is now CONNECTED_GLOBAL"|"NetworkManager state is now CONNECTED_SITE")
       WIFI=1 ;;
@@ -23,9 +23,9 @@ while read -r line; do
       WIFI=0 ;;
     "NetworkManager state is now CONNECTED_LOCAL"|"NetworkManager state is now CONNECTING")
       WIFI=2 ;; # connected local
-    "device (rückersdorf_wg): state change: "*" -> disconnected")
+    "device (rückersdorf_wg): state change: "*" -> disconnected"|"device (r__ckersdorf_wg): state change: "*" -> disconnected")
       HOMENET=0 ;;
-    "device (rückersdorf_wg): Activation: successful, device activated.")
+    "device (rückersdorf_wg): Activation: successful, device activated."|"device (r__ckersdorf_wg): Activation: successful, device activated.")
       HOMENET=1 ;;
   esac
   echo "NET$WIFI $VPN $HOMENET"
