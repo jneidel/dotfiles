834bf79b1e28ce2cfb10a0277702dc306b122b32
Jonathan Neidel <jneidel@jneidel.com>
2024-05-12 16:07:42 +0200

    fix(signal): write wrapper script to handle service failure

--- .zsh/apps.zsh
+++ .zsh/apps.zsh
@@ -102,7 +102,7 @@ alias scim="sc-im" # guide: https://www.youtube.com/watch?v=K_8_gazN7h0
 alias sw="date +'%a %b %d'; raw-stopwatch"
 alias license="legit put gpl-3.0"
 alias fcrontabe="fcrontab -e"
-alias sig="signal-to-inbox"
+alias sig="signal-name-resolution-failure inbox"
 alias mi="mediainfo"
 alias bs="bluelight stop"
 
