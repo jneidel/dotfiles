3ed637ad49264fc40f6128c049e3f47e25b7c83b
Jonathan Neidel <jneidel@jneidel.com>
2024-09-12 18:06:58 +0200

    chore(nvim): upgrade tsserver naming deprecation

--- .config/nvim/lua/plugin/lspconfig.lua
+++ .config/nvim/lua/plugin/lspconfig.lua
@@ -52,7 +52,7 @@ return {
     capabilities = cmp_lsp.default_capabilities(capabilities)
 
     -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-    local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
+    local servers = { 'clangd', 'rust_analyzer', 'pyright', 'ts_ls' }
     for _, lsp in ipairs(servers) do
       lspconfig[lsp].setup {
         -- on_attach = my_custom_on_attach,
