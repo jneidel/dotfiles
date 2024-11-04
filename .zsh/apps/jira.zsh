aa67d18c4f51e046a0a026166e6874c1741494d4
Jonathan Neidel <jonathan.neidel@endava.com>
2024-11-04 14:40:02 +0100

    fix(zsh): use the right project codes for fjira alias

--- .zsh/apps/jira.zsh
+++ .zsh/apps/jira.zsh
@@ -1,12 +1,16 @@
-alias jl="jira issue list -q'status not in (Rejected, Tested, Approved, \"Ready To Test\") AND Owner in (JNeidel)'"
-# usage:
-# v to view
-# m to change state (move)
+if [ "$(uname)" = "Darwin" ]; then
+  alias jl="jira issue list -q'status not in (Rejected, Tested, Approved, \"Ready To Test\") AND Owner in (JNeidel)'"
+  # usage:
+  # v to view
+  # m to change state (move)
 
-jira(){
-  if [ -n "$1" ]; then
-    fjira "ORFDV001-$1"
-  else
-    fjira -p ORFDV001
-  fi
-}
+  jira(){
+    if [ -n "$1" ]; then
+      fjira "ORFDV001-$1"
+    else
+      fjira -p ORFDV001
+    fi
+  }
+else
+  alias jira="fjira -p JD"
+fi
