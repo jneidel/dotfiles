3e06d73d9db7ebcd01194e7eecae4d129da58d44
Jonathan Neidel <jneidel@jneidel.com>
2024-11-15 13:32:22 +0100

    chore(emacs): cleanup ledger config

--- .config/emacs/lisp/init-ledger.el
+++ .config/emacs/lisp/init-ledger.el
@@ -1,38 +1,22 @@
-;;; init-ledger.el --- Support for the ledger CLI accounting tool -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
+(use-package hledger-mode
+             :config
+             (use-package flycheck-hledger))
 
-;; TODO: port this to ledger-mode
-;; https://hledger.org/editors.html
+;; use hledger-mode on journal files
+(add-to-list 'auto-mode-alist '("\\.journal\\'" . hledger-mode))
 
-(when (maybe-require-package 'hledger-mode)
-  (when (maybe-require-package 'flycheck-hledger)
-    (with-eval-after-load 'flycheck
-      (with-eval-after-load 'hledger-mode
-        (require 'flycheck-hledger))))
+(setq ledger-binary-path "hledger"
+      ledger-highlight-xact-under-point nil
+      ledger-use-iso-dates nil
+      ledger-mode-should-check-version nil)
 
-  ;; also open on hledger journal files
-  (add-to-list 'auto-mode-alist '("\\.journal\\'" . hledger-mode))
+(with-eval-after-load 'hledger-mode
+  (setq tab-width 2)
+  (evil-define-key "normal" ledger-mode-map (kbd "RET") 'open-line)
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-copy-env "LEDGER_FILE")))
 
-  (with-eval-after-load 'hledger-mode
-    (evil-define-key "normal" ledger-mode-map (kbd "RET") 'open-line))
-
-  (setq ledger-highlight-xact-under-point nil
-        ledger-use-iso-dates nil
-        ledger-mode-should-check-version nil)
-
-  (with-eval-after-load 'hledger-mode
-    (setq tab-width 2) 
-    (when (memq window-system '(mac ns))
-      (exec-path-from-shell-copy-env "LEDGER_FILE")))
-
-  (add-hook 'ledger-mode-hook 'goto-address-prog-mode))
-
-;; (add-hook 'ledger-mode-hook
-;;           (lambda ()
-;;             (setq-local tab-always-indent 'complete)
-;;             (setq-local completion-cycle-threshold t)
-;;             (setq-local ledger-complete-in-steps t)))
+(add-hook 'ledger-mode-hook 'goto-address-prog-mode)
 
 ;; no rupee after transaction
 ;; src: https://github.com/narendraj9/hledger-mode/issues/39
@@ -44,23 +28,5 @@
    ((hledger-expecting-rupeep) (insert "    "))
    (t (indent-line-to tab-width))))
 (advice-add 'hledger-indent-account-line :override #'hledger-indent-account-line*)
-
-;; mark + M-x align, not great
-;; https://github.com/narendraj9/hledger-mode/issues/28
-;; leder-mode has this built-in
-;; (add-to-list 'align-rules-list
-;;              `(hledger-accounts
-;;                (regexp . ,(rx (+ space)
-;;                               (+? anything)
-;;                               (group-n 1 space (+ space)
-;;                                        (? ?-)
-;;                                        (+ digit)
-;;                                        (? ?.)
-;;                                        (* digit))))
-;;                (group . 1)
-;;                (spacing . 2)
-;;                (justify . t)
-;;                (separate . entire)
-               ;; (modes . '(hledger-mode))))
 
 (provide 'init-ledger)
