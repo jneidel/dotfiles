4c73ebc9ae337e1cea8f286d7cb51074c0d6055d
Jonathan Neidel <jneidel@jneidel.com>
2024-11-15 13:41:52 +0100

    feat(emacs): add binding for adjusting font size

--- .config/emacs/lisp/init-bindings.el
+++ .config/emacs/lisp/init-bindings.el
@@ -1,4 +1,13 @@
 (use-package general) ;; https://github.com/noctuid/general.el#evil-examples
+(use-package hydra) ;; https://github.com/abo-abo/hydra
+
+;; adjust text size
+(defhydra text-size-hydra (global-map "C-+") "Adjust text size"
+          ("+" text-scale-increase "bigger")
+          ("-" text-scale-decrease "smaller")
+          ("=" (text-scale-set 0) "set default")
+          ("d" (text-scale-set 0) "set default")
+          ("f" (text-scale-set 6) "set far"))
 
 (global-set-key (kbd "C-ö") (lambda () (interactive)(split-window-right) (other-window 1)))
 (global-set-key (kbd "C--") (lambda () (interactive)(split-window-below) (other-window 1)))
