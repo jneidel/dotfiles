47be63983189c7c26ca612925878ff24d1fb5ecf
Jonathan Neidel <jneidel@jneidel.com>
2024-11-15 13:48:52 +0100

    feat(emacs): fix and cleanup theme configuration

--- .config/emacs/lisp/init-themes.el
+++ .config/emacs/lisp/init-themes.el
@@ -1,32 +1,19 @@
-;;; init-themes.el --- Defaults for themes -*- lexical-binding: t -*-
-
 (load-theme 'an-old-hope t)
 
+;; Don't prompt to confirm theme safety
+(setq custom-safe-themes t)
+
 ;; Toggle between light and dark
-(defun light ()
-  "Activate a light color theme."
+(defun light-mode()
+  "Activate the light color theme."
   (interactive)
-  (setq custom-enabled-themes '(leuven))
-  (reapply-themes))
+  (use-package modus-themes
+               :config
+               (load-theme 'modus-operandi-tritanopia)))
 
-(defun dark ()
-  "Activate a dark color theme."
+(defun dark-mode()
+  "Activate the dark color theme."
   (interactive)
-  (setq custom-enabled-themes '(an-old-hope))
-  (reapply-themes))
-
-;; (when (maybe-require-package 'dimmer)
-;;   (setq-default dimmer-fraction 0.15)
-;;   (add-hook 'after-init-hook 'dimmer-mode)
-;;   (with-eval-after-load 'dimmer
-;;     ;; TODO: file upstream as a PR
-;;     (advice-add 'frame-set-background-mode :after (lambda (&rest args) (dimmer-process-all))))
-;;   (with-eval-after-load 'dimmer
-;;     ;; Don't dim in terminal windows. Even with 256 colours it can
-;;     ;; lead to poor contrast.  Better would be to vary dimmer-fraction
-;;     ;; according to frame type.
-;;     (defun sanityinc/display-non-graphic-p ()
-;;       (not (display-graphic-p)))
-;;     (add-to-list 'dimmer-exclusion-predicates 'sanityinc/display-non-graphic-p)))
+  (load-theme 'an-old-hope))
 
 (provide 'init-themes)
