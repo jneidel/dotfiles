b64fe8ca1ac50750a5a28010ec15e83834acf412
Jonathan Neidel <jneidel@jneidel.com>
2024-12-11 11:00:23 +0100

    fix(misc): add various small fixes and improvements

--- .zsh/mac.zsh
+++ .zsh/mac.zsh
@@ -1,5 +1,5 @@
 if [ "$(uname)" = "Darwin" ]; then
-  alias brew="HOMEBREW_NO_INSTALL_CLEANUP=1 HOMEBREW_NO_AUTO_UPDATE=1 /usr/local/bin/brew"
+  alias brew="https_proxy= HOMEBREW_NO_INSTALL_CLEANUP=1 HOMEBREW_NO_AUTO_UPDATE=1 /usr/local/bin/brew"
 
   export https_proxy=127.0.0.1:7080
 
