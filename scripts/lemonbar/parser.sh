ae0c3d8e4dcc499f00e7fa450217f796d3fd1249
Jonathan Neidel <jneidel@jneidel.com>
2026-08-29 10:11:01 +0200

    feat(lemonbar): change bar color high cpu/load or low battery

--- scripts/lemonbar/parser.sh
+++ scripts/lemonbar/parser.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . "$DIR/config"
 
 # parse fifo
-while read -r line ; do
+while read -r line; do
   case $line in
     SYS*) # conky
       sys_arr=(${line#???})
@@ -13,23 +13,32 @@ while read -r line ; do
       datetime="${sys_arr[0]} ${sys_arr[1]} ${sys_arr[2]} ${sys_arr[3]}" # dotw, date, month, time
       datetime="%{B${col_sec}}${sep_left} %{T2}${datetime}%{T-} ${sep_right}"
       # cpu
+      cpu_color=$col_main
+      if [ "$(echo ${sys_arr[4]} | cut -d% -f1)" -gt 90 ] || # cpu >90%
+           [ "$(echo ${sys_arr[5]} | cut -d. -f1)" -gt 10 ]; then # load >10.0
+        cpu_color=$col_sec
+      fi
       cpu="${sys_arr[4]} ${sys_arr[5]}" # cpu, load
-      cpu="%{B${col_main}}${sep_left}  %{T2}${cpu}%{T-} ${sep_right}"
+      cpu="%{B${cpu_color}}${sep_left}  %{T2}${cpu}%{T-} ${sep_right}"
       # diskspace
       diskspace="${sys_arr[6]}"
       diskspace="%{B${col_main}}${sep_left}  %{T2}${diskspace}%{T-} ${sep_right}"
 
+      battery_color=$col_main
+      if [ "$(battery.sh percent)" -lt 15 ]; then
+        battery_color=$col_sec
+      fi
       if [ "${sys_arr[7]}" = "G" ]; then # mysterious G dirupts the order
         # battery
         battery="${sys_arr[8]} %{T2}${sys_arr[9]}%%{T-}" # icon, percentage
         battery="%{B${col_main}}${sep_left} ${battery} ${sep_right}"
         # network monitor
         downspeed="${sys_arr[10]}${sys_arr[11]}" # speed, unit
-        downspeed="%{B${col_main}}${sep_left} ﲐ %{T2}${downspeed}%{T-} ${sep_right}"
+        downspeed="%{B${battery_color}}${sep_left} ﲐ %{T2}${downspeed}%{T-} ${sep_right}"
       else
         # battery
         battery="${sys_arr[7]} %{T2}${sys_arr[8]}%%{T-}" # icon, percentage
-        battery="%{B${col_main}}${sep_left} ${battery} ${sep_right}"
+        battery="%{B${battery_color}}${sep_left} ${battery} ${sep_right}"
         # todo: change foreground to red if battery < 5%, same for load/cpu
         # network monitor
         downspeed="${sys_arr[9]}${sys_arr[10]}" # speed, unit
@@ -61,22 +70,22 @@ while read -r line ; do
         fi
       fi
       network="$network%{B$col_sec}"
-      ;;
-    WSP*) # i3 workspaces
-      wsp="" # reset previous content
-      set -- ${line#???}
-      while [ $# -gt 0 ]; do
-        case $1 in
-         FOC*)
-           wsp="${wsp}%{B${col_active}}${sep_left_rev} %{T2}$(echo ${1#???} | cut -d: -f2)%{T-} ${sep_right_rev}"
-           ;;
-         INA*|URG*|ACT*)
-           wsp="${wsp}%{B${col_main}}${sep_left_rev} %{T2}$(echo ${1#???} | cut -d: -f2)%{T-} ${sep_right_rev}"
-           ;;
-        esac
-        shift
-      done
-      ;;
+          ;;
+        WSP*) # i3 workspaces
+          wsp="" # reset previous content
+          set -- ${line#???}
+          while [ $# -gt 0 ]; do
+            case $1 in
+              FOC*)
+                wsp="${wsp}%{B${col_active}}${sep_left_rev} %{T2}$(echo ${1#???} | cut -d: -f2)%{T-} ${sep_right_rev}"
+                ;;
+              INA*|URG*|ACT*)
+                wsp="${wsp}%{B${col_main}}${sep_left_rev} %{T2}$(echo ${1#???} | cut -d: -f2)%{T-} ${sep_right_rev}"
+                ;;
+            esac
+            shift
+          done
+          ;;
   esac
 
   # bar ends
